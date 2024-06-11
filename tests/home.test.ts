f2fc37fff621ea129a26980c7f4984f565cb93ff
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-11 18:14:22 +0200

    tests: updates

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -1,39 +1,45 @@
 import { expect, test } from '@playwright/test';
 
+const title = 'Svelte Flags'
+const description = '250+ country flag icon components for Svelte'
+const keywords = 'svelte, runes, country flag icons, icons, svg, sveltekit'
+const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-flags'
+
 test.beforeEach(async ({ page }) => {
   console.log(`Running ${test.info().title}`);
   await page.goto('/');
 });
 
 test('index page has expected h1', async ({ page }) => {
-	await expect(page.getByRole('heading', { name: 'Svelte Flags', level: 1 })).toBeVisible();
+	await expect(page.getByRole('heading', { name: title, level: 1 })).toBeVisible();
 });
 
 test('index page has expected meta title', async ({ page }) => {
-  await expect(page).toHaveTitle('Svelte Flags');
+  await expect(page).toHaveTitle(title);
 });
 
 test('index page has expected meta description', async ({ page }) => {
   const metaDescription = page.locator('meta[name="description"]');
-  await expect(metaDescription).toHaveAttribute('content', '250+ country flag icon components for Svelte');
+  await expect(metaDescription).toHaveAttribute('content', description);
 });
 
 test('index page has expected meta keywords', async ({ page }) => {
   const metaKeywords = page.locator('meta[name="keywords"]');
-  await expect(metaKeywords).toHaveAttribute('content', 'svelte, runes, country flag icons, icons, svg, sveltekit');
+  await expect(metaKeywords).toHaveAttribute('content', keywords);
 });
 
-test('index page has expected meta og', async ({ page }) => {
+test('index page has expected meta og', async ({ page, baseURL }) => {
   const metaOgTitle = page.locator('meta[property="og:title"]');
-  await expect(metaOgTitle).toHaveAttribute('content', 'Svelte Flags');
+  await expect(metaOgTitle).toHaveAttribute('content', title);
   const metaOgDescription = page.locator('meta[property="og:description"]');
-  await expect(metaOgDescription).toHaveAttribute('content', '250+ country flag icon components for Svelte');
+  await expect(metaOgDescription).toHaveAttribute('content', description);
   const metaOgUrl = page.locator('meta[property="og:url"]');
-  await expect(metaOgUrl).toHaveAttribute('content', 'http://localhost:4173/');
+  const url = baseURL + '/';
+  await expect(metaOgUrl).toHaveAttribute('content', url);
   const metaOgImage = page.locator('meta[property="og:image"]');
   await expect(metaOgImage).toHaveAttribute(
     'content',
-    'https://open-graph-vercel.vercel.app/api/svelte-flags'
+    imgUrl
   );
 });
 
@@ -45,6 +51,6 @@ test('index page has expected meta twitter', async ({ page }) => {
   const metaTwitterImage = page.locator('meta[name="twitter:image"]');
   await expect(metaTwitterImage).toHaveAttribute(
     'content',
-    'https://open-graph-vercel.vercel.app/api/svelte-flags'
+    imgUrl
   );
 });
